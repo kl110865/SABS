b508536ec41ee6054d66a27518b2c3708260c382
Yang <31492969+yangshengf@users.noreply.github.com>
2017-09-10 14:51:40 -0700

    Added Box to Display Total Account Summary

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -17,7 +17,13 @@ export class AccountComponent {
     this.sampleAccountData = [
       { id: 1, name: 'MCD', date: Date.now(), amount: 200, category: 'Food' },
       { id: 2, name: 'Mall', date: Date.now(), amount: 100, category: 'Food' },
-      { id: 3, name: 'Roll', date: Date.now(), amount: 1200, category: 'Game' }
+      { id: 3, name: 'Roll', date: Date.now(), amount: 44, category: 'Game' },
+      { id: 4, name: 'Roll', date: Date.now(), amount: 60, category: 'Game' },
+      { id: 5, name: 'Roll', date: Date.now(), amount: 44, category: 'Game' },
+      { id: 6, name: 'Roll', date: Date.now(), amount: 10, category: 'Game' },
+      { id: 7, name: 'Roll', date: Date.now(), amount: 80, category: 'Game' },
+      { id: 8, name: 'Roll', date: Date.now(), amount: 44, category: 'Game' },
+      { id: 9, name: 'Roll', date: Date.now(), amount: 44, category: 'Game' },
     ];
   }
 
